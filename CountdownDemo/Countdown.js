ccfc26fd148c769d8e0ca3421d5049ed89382152
Prosper Ughakpoteni <108951615+Prosperteni@users.noreply.github.com>
2024-10-30 20:07:10 +0100

    Add files via upload

---
+++ CountdownDemo/Countdown.js
@@ -0,0 +1,24 @@
+// Set the date we're counting down to (change this to your desired date)
+const countDownDate = new Date("2024-05-10 00:00:00").getTime();
+
+// Update the countdown every 1 second
+const x = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    // If the countdown is over, display a message
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+}, 1000);
